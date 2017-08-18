0395c9442cb39c81951481f632c386e553238ac7
Hieu Do <mr.hieudt@gmail.com>
2017-08-17 20:33:48 -0500

    Implement relation between user and company types

--- schema/schema.js
+++ schema/schema.js
@@ -5,16 +5,40 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
   GraphQLSchema,
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      async resolve({ id }) {
+        const { data: users } = await axios.get(`http://localhost:3000/companies/${id}/users`);
+        return users;
+      },
+    },
+  }),
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-  },
+    company: {
+      type: CompanyType,
+      async resolve({ companyId }) {
+        const { data: company } = await axios.get(`http://localhost:3000/companies/${companyId}`);
+        return company;
+      },
+    },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -23,9 +47,17 @@ const RootQuery = new GraphQLObjectType({
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
-      async resolve(parentValue, args) {
-        const { data: user } = await axios.get(`http://localhost:3000/users/${args.id}`);
+      async resolve(_, { id: userId }) {
+        const { data: user } = await axios.get(`http://localhost:3000/users/${userId}`);
         return user;
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      async resolve(_, { id: companyId }) {
+        const { data: company } = await axios.get(`http://localhost:3000/companies/${companyId}`);
+        return company;
       },
     },
   },
