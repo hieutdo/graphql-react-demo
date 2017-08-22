01ec636f09ab2bf43900886411c86a9b18658cd2
Hieu Do <mr.hieudt@gmail.com>
2017-08-22 10:46:59 -0500

    Create user model

---
+++ server/src/models/user.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: String,
+  password: String,
+});
+
+// TODO convert all callbacks to promise using util.promisify
+UserSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
