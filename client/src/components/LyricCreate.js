c07ef4a070b793cfdc7058c74a1c944b7dd63805
Hieu Do <mr.hieudt@gmail.com>
2017-08-22 08:19:33 -0500

    Fix issue not updating song's lyrics after adding one

--- client/src/components/LyricCreate.js
+++ client/src/components/LyricCreate.js
@@ -35,8 +35,9 @@ class LyricCreate extends Component {
 const mutation = gql`
   mutation AddLyricToSong($content: String, $songId: ID) {
     addLyricToSong(content: $content, songId: $songId) {
-      id,
+      id
       lyrics {
+        id
         content
       }
     }
