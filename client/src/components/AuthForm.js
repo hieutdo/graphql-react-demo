fea3c3e11f03491dfe2e394663864fc31ebb46bc
Hieu Do <mr.hieudt@gmail.com>
2017-08-22 21:49:34 -0500

    Display errors on login form

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -35,6 +35,9 @@ class AuthForm extends Component {
               onChange={e => this.setState({ password: e.target.value })}
             />
           </div>
+          <div className="errors">
+            {this.props.errors.map(e => <div key={e}>{e}</div>)}
+          </div>
           <button type="submit" className="btn">Login</button>
         </form>
       </div>
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -5,18 +5,31 @@ import login from '../queries/login';
 import currentUser from '../queries/current-user';
 
 class LoginForm extends Component {
-  onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { email, password },
-      refetchQueries: [{ query: currentUser }],
-    });
+  constructor(props) {
+    super(props);
+    this.state = { errors: [] };
+  }
+
+  async onSubmit({ email, password }) {
+    try {
+      await this.props.mutate({
+        variables: { email, password },
+        refetchQueries: [{ query: currentUser }],
+      });
+    } catch (e) {
+      const errors = e.graphQLErrors.map(err => err.message);
+      this.setState({ errors });
+    }
   }
 
   render() {
     return (
       <div>
         <h4>Login</h4>
-        <AuthForm onSubmit={this.onSubmit.bind(this)} />
+        <AuthForm
+          errors={this.state.errors}
+          onSubmit={this.onSubmit.bind(this)}
+        />
       </div>
     );
   }
